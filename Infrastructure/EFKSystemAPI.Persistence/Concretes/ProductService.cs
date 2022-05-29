e650545933bc5aa39d4b22aa8f0af41e7e9d6487
Emre Karaloğlu <emrekaraloglu@hotmail.com>
2022-05-29 17:47:35 +0300

    Add project files.

---
+++ Infrastructure/EFKSystemAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,22 @@
+﻿using EFKSystemAPI.Application.Abstractions;
+using EFKSystemAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFKSystemAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+         => new()
+         {
+             new() { Id = Guid.NewGuid(), Name = "Product Test-1", Price = 100, CreateDate = DateTime.Now, Stock = 10 },
+             new() { Id = Guid.NewGuid(), Name = "Product Test-2", Price = 200, CreateDate = DateTime.Now, Stock = 20 },
+             new() { Id = Guid.NewGuid(), Name = "Product Test-3", Price = 300, CreateDate = DateTime.Now, Stock = 30 },
+             new() { Id = Guid.NewGuid(), Name = "Product Test-4", Price = 400, CreateDate = DateTime.Now, Stock = 40 }
+         };
+    }
+}
---
+++ Presentation/EFKSystemAPI.BuildAPI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using EFKSystemAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFKSystemAPI.BuildAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var data = _productService.GetProducts();
+            return Ok(data);
+        }
+    }
+}
