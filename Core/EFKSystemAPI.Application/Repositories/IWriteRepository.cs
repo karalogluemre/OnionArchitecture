529e8539370fc994dba07cacc5c74a6fe2f4c495
Emre <Emre@teknohub.com.tr>
2022-06-07 10:09:21 +0300

    Part-3 Repository

---
+++ Core/EFKSystemAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFKSystemAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        Task<bool> SaveAsync(T entity);
+        Task<bool> SaveAsync(List<T> entity);
+        Task<bool> Remove(T entity);
+        Task<bool> Remove(string id);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
---
+++ Infrastructure/EFKSystemAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,35 @@
+﻿using EFKSystemAPI.Application.Repositories;
+using EFKSystemAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFKSystemAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReadRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAllAsync() => Table;
+
+        public Task<T> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetSingleAsync(Expression<Func<T, bool>> method) => await Table.FirstOrDefaultAsync(method);
+
+        public IQueryable<T> GetWhereAsync(Expression<Func<T, bool>> method) => Table.Where(method);
+    }
+}
