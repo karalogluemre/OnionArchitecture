0a2473fa8e27cc7fa8276165fc4b9401642f0079
Emre <Emre@teknohub.com.tr>
2022-06-14 15:58:15 +0300

    Part-8

--- Presentation/EFKSystemAPI.BuildAPI/Controllers/ProductsController.cs
+++ Presentation/EFKSystemAPI.BuildAPI/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using EFKSystemAPI.Application.Abstractions;
 using EFKSystemAPI.Application.Repositories.Products;
+using EFKSystemAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,11 +20,22 @@ namespace EFKSystemAPI.BuildAPI.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productsWriteRepository.SaveRangeAsync(new()
-            {
-                new() { Id = Guid.NewGuid(), Name = "Product-1", Price = 200, CreateDate = DateTime.UtcNow, Stock = 30 }
-            });
-            var count = await _productsWriteRepository.SaveAsync();
+            //await _productsWriteRepository.SaveRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name = "Product-1", Price = 200, CreateDate = DateTime.UtcNow, Stock = 30 }
+            //});
+            //var count = await _productsWriteRepository.SaveAsync();
+            Product p = await _productsReadRepository.GetByIdAsync("id gelecek");
+            //Product p = await _productsReadRepository.GetByIdAsync("id gelecek",false);// p.Name 'i değiştirildiği zaman Ahmet'i Mehmet yaparsak güncellemez. Tracking her sorguda takip edilen değeri değiştirir.False yaparsak takip edilen bu değer değişime kapalı olur.
+            p.Name = "Ahmet";
+            await _productsWriteRepository.SaveAsync();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productsReadRepository.GetByIdAsync(id);
+            return Ok(product);
         }
     }
 }
